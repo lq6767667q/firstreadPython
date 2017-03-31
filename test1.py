a921cee796a27d98830afff03cb1392a685d6f9a
liurui <liurui_777777@163.com>
2017-03-31 22:29:37 +0800

    1

---
+++ test1.py
@@ -0,0 +1,13 @@
+# test1.py
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print('hello,world中午')
+name = input('input:...')
+print(name)
+print('1024 * 768 =',1024 * 768)
+
+s1 = 72
+s2 = 85
+r = (s2-s1)/s1*100
+print('%.1f%%' % r)
+print('after add github')
